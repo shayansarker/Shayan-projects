033977362ccd500467e0a851b98d9305e6dd0f7c
ShayanSarker <121078840+shayansarker@users.noreply.github.com>
2024-02-02 14:36:50 +0600

    Update new.ino

--- new.ino
+++ new.ino
@@ -2,24 +2,24 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-// Set the LCD address to 0x27 for a 16x2 display
+
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 //By Shayan Sarker
 void setup() {
-  lcd.backlight();  // Turn on the backlight
+  lcd.backlight();  
 }
 
 //By Shayan Sarker
 void loop() {
-  static const char staticText[] = "By Shayan Sarker";
-  static char scrollingText[] = "Grade:IV Section:A(boys)";
+  static const char staticText[] = "messi";
+  static char scrollingText[] = "suiiiiiiiCR7";
 
-  // Display static text on the first line
+
   lcd.setCursor(0, 0);
   lcd.print(staticText);
 
-  // Scroll the text on the second line
+
   for (int i = 0; i < strlen(scrollingText) + 16; i++) {
     lcd.setCursor(max(0, 15 - i), 1);
     lcd.print(scrollingText + max(0, i - 16));
